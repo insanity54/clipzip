3674fe7545ddf435483259f373e05c1ce47fc823
Chris Grimmett <chris@grimtech.net>
2022-11-19 13:56:28 -0800

    fix parseTwitterHandle so it catches names with numbers

---
+++ video/test/socialMedia.test.js
@@ -0,0 +1,79 @@
+
+const expect = require('chai').expect;
+const { getYouTubeChannelName, getVtuberData } = require('../lib/socialMedia');
+
+
+describe('socialMedia', function () {
+    describe('getYouTubeChannelName', function () {
+        this.timeout(30*1000);
+
+        it('should get a channel name when given a /channel/<MACHINE_ID> URL ', async function () {
+            const name = await getYouTubeChannelName('https://www.youtube.com/channel/UC1yoRdFoFJaCY-AGfD9W0wQ')
+            expect(name).to.equal('Projekt Melody')
+        })
+        it('should get a channel name when given a /c/<HUMAN_READABLE_ID> URL', async function () {
+            const name = await getYouTubeChannelName('https://www.youtube.com/c/ApricottheLichTwitchArchive')
+            expect(name).to.equal('ApricottheLichTwitchArchive');
+        })
+    })
+
+
+  describe('getVtuberData', function () {
+
+    it('getVtuberData ironmouse', async function () {
+      this.timeout(1000*60*2)
+       
+      const data = await getVtuberData('ironmouse', true);
+      expect(data).to.have.property('socialMediaLinks');
+      expect(data).to.have.property('displayName');
+      expect(data.displayName).to.equal('ironmouse');
+      expect(data.socialMediaLinks).to.have.lengthOf.above(0);
+      setTimeout(() => { return }, 2000)
+    })
+    it('getVtuberData silvervale', async function () {
+      this.timeout(1000*60*2)
+       
+      const data = await getVtuberData('silvervale', true);
+      expect(data).to.have.property('socialMediaLinks');
+      expect(data).to.have.property('displayName');
+      expect(data.displayName).to.equal('Silvervale');
+      expect(data.socialMediaLinks).to.have.lengthOf.above(0);
+      expect(data.socialMediaLinks[0]).to.be.a('string');
+      setTimeout(() => { return }, 2000)
+    })
+    it('getVtuberData kuzuryuio', async function () {
+      this.timeout(1000*60*2)
+       
+      const data = await getVtuberData('kuzuryuio', true);
+      expect(data).to.have.property('socialMediaLinks');
+      expect(data).to.have.property('displayName');
+      expect(data.displayName).to.equal('KuzuryuIo');
+      expect(data.socialMediaLinks).to.have.lengthOf.above(0);
+      expect(data.socialMediaLinks[0]).to.be.a('string');
+      setTimeout(() => { return }, 2000)
+    })
+    it('getVtuberData apricot', async function () {
+      this.timeout(1000*60*2)
+       
+      const data = await getVtuberData('apricot', true);
+      expect(data).to.have.property('socialMediaLinks');
+      expect(data).to.have.property('displayName');
+      expect(data.displayName).to.equal('Apricot');
+      expect(data.socialMediaLinks).to.have.lengthOf.above(0);
+      expect(data.socialMediaLinks[0]).to.be.a('string');
+      setTimeout(() => { return }, 2000)
+    })
+    it('getVtuberData zentreya', async function () {
+      this.timeout(1000*60*2)
+       
+      const data = await getVtuberData('zentreya', true);
+      expect(data).to.have.property('socialMediaLinks');
+      expect(data).to.have.property('displayName');
+      expect(data.displayName).to.equal('Zentreya');
+      expect(data.socialMediaLinks).to.have.lengthOf.above(0);
+      expect(data.socialMediaLinks[0]).to.be.a('string');
+      setTimeout(() => { return }, 2000)
+    })
+  })
+
+});
---
+++ video/test/upload.test.js
@@ -0,0 +1,87 @@
+
+require('dotenv').config();
+const Uploader = require('../lib/upload');
+const path = require('path');
+const envImport = require('@grimtech/envimport');
+const PORT = envImport('PORT');
+const expect = require('chai').expect;
+const { parseTwitterHandle } = require('../lib/socialMedia');
+
+const vtuberData = { 
+  name: 'ironmouse', 
+  youtube: 'https://www.youtube.com/channel/UChgPVLjqugDQpRLWvC7zzig', 
+  twitter: 'https://twitter.com/ironmouse/', 
+  discord: 'https://discord.com/invite/preciousfamily',
+  tiktok: 'https://www.tiktok.com/@ironmouse?',
+  patreon: 'https://www.patreon.com/ironmouse',
+  twitch: 'https://www.twitch.tv/ironmouse', 
+  sharkrobot: 'https://sharkrobot.com/collections/vshojo-store'
+};
+
+const socialMediaLinks = [
+  'https://www.youtube.com/c/ApricottheLichVS',
+  'https://twitter.com/LichVtuber',
+  'https://www.youtube.com/c/ApricottheLichTwitchArchive'
+];
+
+const testVideo = path.join(__dirname, '..', 'output', '1617878726815.mp4');
+const testDate = '2021-02-01';
+const videoIdRegex = /[\w\d-_]{11}/;
+
+describe('uploader', () => {
+
+  describe('getTwitterHandle', function () {
+    it('should return empty string if no twitter exists in the socialMediaLinks', function () {
+      const socialMediaLinksFixture = [
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+      ]
+    })
+    it('should accept an array of socialMediaLinks and return a twitter handle if it exists', function () {
+      const socialMediaLinksFixture = [
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        'https://ko-fi.com/cj_clippy',
+        'https://twitter.com/cj_clippy'
+      ]
+      expect(Uploader.getTwitterHandle(socialMediaLinksFixture)).to.equal('@cj_clippy')
+    })
+  })
+  describe('parseTwitterHandle', function () {
+    it('should find a twitter @ handle in a URL of any variety', function () {
+      const expectedHandle = '@ironmouse';
+      expect(parseTwitterHandle('ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('@ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('https://twitter.com/ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('https://twitter.com/@ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('http://twitter.com/ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('http://twitter.com/@ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('twitter.com/@ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('twitter.com/ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('https://www.twitter.com/intent/follow?screen_name=ironmouse')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('https://twitter.com/ironmouse?s=09')).to.equal(expectedHandle);
+      expect(parseTwitterHandle('https://twitter.com/2AM_mocha')).to.equal('@2AM_mocha');
+    })
+    it('should strip out query parameters that streamers might have put there', function () {
+      const expectedHandle = '@kaiourachu';
+      expect(parseTwitterHandle('https://twitter.com/intent/user?screen_name=kaiourachu')).to.equal(expectedHandle);
+    })
+  })
+  describe('upload', async function () {
+    it('should return a yt video ID', async function () {
+      this.timeout(1000*60*10)
+      const uploader = new Uploader()
+      const id = await uploader.upload({ channel: 'ironmouse', videoFile: testVideo, date: testDate });
+      expect(id).to.match(/[a-zA-Z0-9_-]/);
+    });
+  })
+  describe('generateVideoDescription', function () {
+    this.timeout(30*1000)
+    it('should tag the vtuber\'s YT channel, if there is a yt channel in the data.', async function () {
+      const uploader = new Uploader();
+      const output = await uploader.generateVideoDescription('apricot', socialMediaLinks);
+      expect(output).to.match(/  \* @Apricot the Lich\n/);
+      expect(output).to.match(/  \* @Apricot the Lich Twitch Archive\n/);
+      expect(output).to.match(/  \* https:\/\/twitch.tv\/apricot/)
+    })
+  })
+
+})
